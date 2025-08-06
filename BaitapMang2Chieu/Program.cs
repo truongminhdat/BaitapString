4ec5ae96175df0ec121045aaf59aa1fbba65f976
Truongminhdat789@gmail.com <Truongminhdat789@gmail.com>
2025-08-06 14:42:16 +0700

    BT

---
+++ BaitapMang2Chieu/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhập số dòng m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhập số cột n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] A = new int[m, n];
+
+        Console.WriteLine("Nhập các phần tử của ma trận A:");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"A[{i},{j}] = ");
+                A[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("\nMa trận A:");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(A[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        int tich = 1;
+        bool coBoiso3 = false;
+        for (int j = 0; j < n; j++)
+        {
+            if (A[0, j] % 3 == 0)
+            {
+                tich *= A[0, j];
+                coBoiso3 = true;
+            }
+        }
+
+        if (coBoiso3)
+        {
+            Console.WriteLine($"\nTích các số là bội số của 3 trên dòng đầu tiên: {tich}");
+        }
+        else
+        {
+            Console.WriteLine("\nKhông có số nào là bội số của 3 trên dòng đầu tiên.");
+        }
+
+        int[] X = new int[m];
+        for (int i = 0; i < m; i++)
+        {
+            int max = A[i, 0];
+            for (int j = 1; j < n; j++)
+            {
+                if (A[i, j] > max)
+                {
+                    max = A[i, j];
+                }
+            }
+            X[i] = max;
+        }
+
+        Console.WriteLine("\nMảng X chứa giá trị lớn nhất của từng dòng:");
+        for (int i = 0; i < m; i++)
+        {
+            Console.Write(X[i] + " ");
+        }
+    }
+}
---
+++ BaitapMang2ChieuBai2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // ======= HÌNH 1 =======
+        Console.WriteLine("Hình 1:\n");
+
+        int h1_rows = 7; 
+        int h1_cols = 6; 
+
+        char[,] h1 = new char[h1_rows, h1_cols];
+
+        for (int i = 0; i < h1_rows; i++)
+        {
+            for (int j = 0; j < h1_cols; j++)
+            {
+                if (i == 0 || i == h1_rows - 1 || j == 0 || j == h1_cols - 1)
+                {
+                    h1[i, j] = '*';
+                }
+                else
+                {
+                    h1[i, j] = ' ';
+                }
+            }
+        }
+
+        // In hình 1
+        for (int i = 0; i < h1_rows; i++)
+        {
+            for (int j = 0; j < h1_cols; j++)
+            {
+                Console.Write(h1[i, j]);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("--------------" +
+            ""); 
+
+        // ======= HÌNH 2 =======
+        Console.WriteLine("Hình 2:\n");
+
+        int h2_size = 6;
+        char[,] h2 = new char[h2_size, h2_size];
+
+        for (int i = 0; i < h2_size; i++)
+        {
+            for (int j = 0; j < h2_size; j++)
+            {
+                if (i == 0 || i == h2_size - 1) 
+                {
+                    h2[i, j] = '*';
+                }
+                else if (j == i) 
+                {
+                    h2[i, j] = '*';
+                }
+                else
+                {
+                    h2[i, j] = ' ';
+                }
+            }
+        }
+
+        // In hình 2
+        for (int i = 0; i < h2_size; i++)
+        {
+            for (int j = 0; j < h2_size; j++)
+            {
+                Console.Write(h2[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
